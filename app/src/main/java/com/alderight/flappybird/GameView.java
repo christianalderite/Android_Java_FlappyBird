570a6534c25284db61bb97bb9af1eed3ef669ad5
Christian Nicole Alderite <christian_alderite@dlsu.edu.ph>
2022-01-30 14:53:24 +0800

    added bird motion mechanics, made the play button transparent

--- app/src/main/java/com/alderight/flappybird/GameView.java
+++ app/src/main/java/com/alderight/flappybird/GameView.java
@@ -9,6 +9,7 @@ import android.graphics.Point;
 import android.graphics.Rect;
 import android.os.Handler;
 import android.view.Display;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class GameView extends View{
@@ -26,6 +27,9 @@ public class GameView extends View{
     Bitmap[] birds;
     // We need an integer variable to keep track of bird frame
     int birdFrame = 0;
+    int velocity = 0, gravity = 3; // Lets play around with these values
+    // We need to keep track of bird position
+    int birdX, birdY;
 
     public GameView(Context context){
         super(context);
@@ -47,7 +51,8 @@ public class GameView extends View{
         birds = new Bitmap[2];
         birds[0] = BitmapFactory.decodeResource(getResources(), R.drawable.bird1);
         birds[1] = BitmapFactory.decodeResource(getResources(), R.drawable.bird2);
-
+        birdX = dWidth/2 - birds[0].getWidth()/2; // Initially bird will be on center
+        birdY = dHeight/2 - birds[0].getHeight()/2;
     }
 
     @Override
@@ -62,9 +67,31 @@ public class GameView extends View{
         }else{
             birdFrame = 0;
         }
+
+        // The bird should be on the screen
+        if(birdY < dHeight - birds[0].getHeight() || velocity < 0){ // This way bird does not go beyond the bottom edge of the screen
+            velocity += gravity; // As the bird falls, it gets faster and faster as the velocity value increases by gravity each time
+            birdY += velocity;
+        }
+
         // We want the bird to be displayed at the center of the screen
         // Both birds[0] and birds[1] have same dimension
-        canvas.drawBitmap(birds[birdFrame], dWidth/2 - birds[0].getWidth()/2, dHeight/2 - birds[0].getHeight()/2, null);
+        canvas.drawBitmap(birds[birdFrame], birdX, birdY, null);
         handler.postDelayed(runnable,UPDATE_MILLIS);
+    }
+    // Get the touch event
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        int action = event.getAction();
+        if(action == MotionEvent.ACTION_DOWN) {
+            // This is tap is detected on screen
+            // Here we want the bird to move upwards by some unit
+            velocity = -30; // Lets say 30 units on upward direction
+
+        }
+
+        return true; // By return true indicates that we're done with touch event and no further action is required by Android
     }
 }
